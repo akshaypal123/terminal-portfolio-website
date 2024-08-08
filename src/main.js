fa142b40c7cd7dd236d2b465d8c3c0ae368e5a66
Akshay Pal <akshay.pal@digital.homeoffice.gov.uk>
2024-08-08 09:52:46 +0100

    fixed resume path issue

--- src/main.js
+++ src/main.js
@@ -66,7 +66,7 @@ Object.defineProperty(Constants, "RESUME_PATH", {
     enumerable: true,
     configurable: true,
     writable: true,
-    value: 'https://1drv.ms/b/s!AiISxQfw7jnKgeIszW13hdU66LKDnw'
+    value: '/../public/resume.pdf'
 });
 Object.defineProperty(Constants, "GUI_WEBSITE_URL", {
     enumerable: true,
--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ abstract class Constants {
     `
   Let's chat! You can email me at <a href=mailto:akshay.anu@gmail.com>akshay.anu@gmail.com</a>
   `;
-  static readonly RESUME_PATH: string = 'https://1drv.ms/b/s!AiISxQfw7jnKgeIszW13hdU66LKDnw';
+  static readonly RESUME_PATH: string = '/../public/resume.pdf';
   static readonly GUI_WEBSITE_URL: string = 'https://bbc.com';
 }
 
